4c46481887d0c3afa9d207195c486ed1fd8cb48d
dtneves <dneves@di.uminho.pt>
2022-01-30 22:03:22 +0100

    Create exception.py

---
+++ purify/exception.py
@@ -0,0 +1,65 @@
+########################################################################################################################
+# Research Centers
+# ----------------
+# Centro ALGORITMI - School of Engineering – University of Minho
+# Braga - Portugal
+# http://algoritmi.uminho.pt/
+#
+# Medical Informatics Group
+# BIH - Berlin Institute of Health
+# Charité - Universitätsmedizin Berlin
+# https://www.bihealth.org/en/research/research-groups/fabian-prasser/
+#
+#
+# Description
+# -----------
+# A tag class to mark exceptions thrown by purify's code.
+#
+#
+# Moto
+# ----
+# "We think too much and feel too little. More than machinery we need humanity."
+#                         -- Excerpt of the final speech from The Great Dictator
+#
+#
+# Authors
+# -------
+# diogo telmo neves -- {dneves@di.uminho.pt, diogo-telmo.neves@charite.de, tada.science@gmail.com}
+#
+#
+# Copyright
+# ---------
+# Copyright (c) 2020 diogo telmo neves.
+# All rights reserved.
+#
+#
+# Conditions
+# ----------
+# This code is free/open source code but the following conditions must be met:
+#   * Redistributions of source code must retain the above copyright notice, this list of conditions and
+#     the following disclaimer in the documentation and/or other materials provided with the distribution.
+#   * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+#     the following disclaimer in the documentation and/or other materials provided with the distribution.
+#
+#
+# DISCLAIMER
+# ----------
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+# TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#
+# Date
+# ----
+# August 2020
+########################################################################################################################
+
+class purifyException(Exception):
+    pass
+
