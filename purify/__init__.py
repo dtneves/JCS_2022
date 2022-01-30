50628531d166a7bcfe539d2da2d092117c473d08
dtneves <dneves@di.uminho.pt>
2022-01-30 21:50:49 +0100

    Create __init__.py

---
+++ purify/__init__.py
@@ -0,0 +1,89 @@
+########################################################################################################################
+# Research Centers
+# ----------------
+# Centro ALGORITMI - School of Engineering – University of Minho
+# Braga - Portugal
+# http://algoritmi.uminho.pt/
+#
+# Medical Informatics Group
+# BIH - Berlin Institute of Health
+# Charité - Universitätsmedizin Berlin
+# https://www.bihealth.org/en/research/research-groups/fabian-prasser/
+#
+#
+# Description
+# -----------
+# purify is a Python package to leverage the implementation of data analysis pipelines.
+# In this repository, only the necessary modules of purify to perform synthetic tabular data generation are exposed.
+#
+#
+# Moto
+# ----
+# "We think too much and feel too little. More than machinery we need humanity."
+#                         -- Excerpt of the final speech from The Great Dictator
+#
+#
+# Authors
+# -------
+# diogo telmo neves -- {dneves@di.uminho.pt, diogo-telmo.neves@charite.de, tada.science@gmail.com}
+#
+#
+# Copyright
+# ---------
+# Copyright (c) 2020 diogo telmo neves.
+# All rights reserved.
+#
+#
+# Conditions
+# ----------
+# This code is free/open source code but the following conditions must be met:
+#   * Redistributions of source code must retain the above copyright notice, this list of conditions and
+#     the following disclaimer in the documentation and/or other materials provided with the distribution.
+#   * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+#     the following disclaimer in the documentation and/or other materials provided with the distribution.
+#
+#
+# DISCLAIMER
+# ----------
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+# TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#
+# Date
+# ----
+# September 2021
+########################################################################################################################
+
+"""Top-level package of `purify`"""
+
+__author__ = 'diogo telmo neves'
+__email__ = 'tada.science@gmail.com'
+__version__ = '1.0.2'
+
+from purify.encoders.encoders import label_encoders_fit_transform, label_encoders_inverse_transform
+from purify.encoders.encoders import get_dummies_fit_transform, get_dummies_inverse_transform
+
+from purify.tabulator import tabulator
+from purify.tabulator import tabulator_CP
+from purify.tabulator import tabulator_GP
+from purify.tabulator import TabularDataGenerator
+
+from purify.exception import purifyException
+
+__all__ = (
+    'label_encoders_fit_transform', 'label_encoders_inverse_transform',
+    'get_dummies_fit_transform', 'get_dummies_inverse_transform',
+    'tabulator',
+    'tabulator_CP',
+    'tabulator_GP',
+    'TabularDataGenerator',
+    'purifyException'
+)
+
